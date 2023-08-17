0be7344e9a9788b72440a02c6a47e4e03a8d0b37
Jessica Odonchimeg <jessicaodonchimeg5@gmail.com>
2023-08-17 09:49:11 -0700

    test

---
+++ Final Project/HI.swift
@@ -0,0 +1,20 @@
+//
+//  HI.swift
+//  Final Project
+//
+//  Created by Jessica Odonchimeg on 8/17/23.
+//
+
+import SwiftUI
+
+struct HI: View {
+    var body: some View {
+        Text("Hello, Antigoni!")
+    }
+}
+
+struct HI_Previews: PreviewProvider {
+    static var previews: some View {
+        HI()
+    }
+}
